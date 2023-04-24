8ff39268b2474fecf07d6be3006cdcb9d66d5f1c
Tony Hayden <haydena23@up.edu>
2023-04-24 16:28:16 -0700

    Improved faciel detection, added eye boxes

--- script.py
+++ script.py
@@ -1,10 +1,21 @@
 import cv2
+import dlib
+
+# To install dlib, run:
+# pip install cmake
+# pip install dlib
+# dlib takes awhile to install
 
 # Set up the video capture object to use your laptop's built-in camera
 cap = cv2.VideoCapture(0)
 
-# Load the face detection classifier
+# Load the face and eye detection classifiers
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Load the facial landmark predictor
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+detector = dlib.get_frontal_face_detector()
 
 while True:
     # Capture a frame from the video stream
@@ -14,19 +25,50 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the grayscale frame
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     print(faces)
+    if(len(faces) == 0):
+        print("No faces")
+    else:
+        # Draw a rectangle around each detected face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
+            # Extract the face region of interest
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
 
-    # Draw a rectangle around each detected face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            text = f"Face Width: {w} | Face Height: {h}"
+            cv2.putText(frame, text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            # Detect eyes in the face region of interest
+            eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+
+            # Draw a rectangle around each detected eye
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
+
+            # Use facial landmarks detection to check the position of the eyes
+            dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+            landmarks = predictor(gray, dlib_rect)
+
+            left_eye_pos = (landmarks.part(36).x, landmarks.part(36).y)
+            right_eye_pos = (landmarks.part(45).x, landmarks.part(45).y)
+
+            cv2.circle(frame, left_eye_pos, 2, (255, 0, 0), 2)
+            cv2.circle(frame, right_eye_pos, 2, (255, 0, 0), 2)
+
+            # If the eyes are too far apart or too close together, remove the face detection
+            eye_dist = right_eye_pos[0] - left_eye_pos[0]
+            if eye_dist > 2 * w or eye_dist < 0.5 * w:
+                faces = []
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
-
     # Check for a key press to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 # Release the video capture object and close the display window
 cap.release()
