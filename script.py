359f6334ada925ba30ca383420331e21046ed634
Tony Hayden <haydena23@up.edu>
2023-04-24 15:29:07 -0700

    Working facial detection using laptop camera

---
+++ script.py
@@ -0,0 +1,33 @@
+import cv2
+
+# Set up the video capture object to use your laptop's built-in camera
+cap = cv2.VideoCapture(0)
+
+# Load the face detection classifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    # Capture a frame from the video stream
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print(faces)
+
+    # Draw a rectangle around each detected face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # Check for a key press to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
